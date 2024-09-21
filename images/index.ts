243cf2e55ca267c160d783f2261fcb4b81670bf3
L1bro <yuraggddff2@gmail.com>
2024-09-21 15:19:16 +0300

    Add files via upload

---
+++ images/index.ts
@@ -0,0 +1,15 @@
+import what from "./what.png";
+import coin from "./coin.png";
+import highVoltage from "./high-voltage.png";
+import secret from "./secret.png";
+import rocket from "./rocket.png";
+import trophy from "./trophy.png";
+
+export {
+    what,
+    coin,
+    highVoltage,
+    secret,
+    rocket,
+    trophy
+}
